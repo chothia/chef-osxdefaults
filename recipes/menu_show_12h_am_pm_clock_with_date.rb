5b141c5cdccbc731b1061b85713b68b0886fb42c
Brandon Chothia <brandon@chothia.com>
2015-03-02 23:19:34 -0800

    Update menu_show_12h_am_pm_clock_with_date.rb

--- recipes/menu_show_12h_am_pm_clock_with_date.rb
+++ recipes/menu_show_12h_am_pm_clock_with_date.rb
@@ -1,5 +1,5 @@
 osxdefaults_defaults "Add date in menubar clock with a 12hour clock with am pm" do
   domain 'com.apple.menuextra.clock'
   key 'DateFormat'
-  string "\"EEE MMM d  h:mm:ss\""
+  string "\"EEE MMM d  h:mm:ss a\""
 end
